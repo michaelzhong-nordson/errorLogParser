00170df0c1a0eae8f1cf521576ba0bbec0cf9451
Michael Zhong <michael.zhong@nordsonasymtek.com>
2017-03-20 16:29:14 -0700

    work in progress

--- errorLogParser.java
+++ errorLogParser.java
@@ -1,12 +1,14 @@
 import java.io.*;
+import javax.swing.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
 //Begin Method
 class errorLogParser {
 	public static void main(String[] args){
-		HashMap <String, HashMap> eventType = new HashMap();
-		HashMap <Sttring, HashMap> 
+		HashMap <String, HashMap> eventType = new HashMap <String, HashMap>();
+		HashMap <String, HashMap> eventStage = new HashMap <String, HashMap>();
+		HashMap <String, String> logmsg = new HashMap<String, String>();
 		JFileChooser fc = new JFileChooser();
 		File input, output;
 		String[] parsed_line;
@@ -14,20 +16,22 @@ class errorLogParser {
 		fc.showOpenDialog(null);
 		input = fc.getSelectedFile();
 
-
-		if(input != null){
+		try{
 			Scanner scan = new Scanner(input);
-
 			while(scan.hasNextLine()){
-				try{
-					line = scan.nextLine().split(" ");
-					System.out.println("Line Array: " + parsed_line);
+				parsed_line = scan.nextLine().split(" ");
+				System.out.println("Line Array: " + parsed_line);
 
-					for()
-				} catch(Exception e){
-					System.out.println(e);
+				for(i = 2; i < parsed_line.length; i++){
+					// Check for unique event types
+					if(!eventType.containsKey(parsed_line[i])){
+						eventType.put(parsed_line[i], new)
+					}
 				}
+
 			}
+		}catch(Exception e){
+					System.out.println(e);
 		}
 	}
 }
